2b29b04d79b062676ca4163bc50f7b2f64a01968
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2022-01-07 18:11:51 -0400

    Update server.js
    - changes imports for requires
    - create connection with database using mongoose
    - activate config for .env files
    - activate express. json() module
    - call subscribers route from proper file

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-//modules imported
-import Express from 'express'
+require('dotenv').config()
 
+const express = require('express')
 //app created
-const app = Express()
+const app = express()
 //Port defined
 const PORT = 3000
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('error', () => console.log('connected to database'))
+
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
 
 //App initiated
 app.listen(PORT, () =>{
